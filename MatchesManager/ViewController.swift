52e11b63192c1ea41765e0f33e1acdb5b5794b54
Nadaf, Imam <imam_nadaf_tp@bmc.com>
2020-05-09 11:59:52 +0530

    Initial Commit

---
+++ MatchesManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MatchesManager
+//
+//  Created by Nadaf, Imam on 09/05/20.
+//  Copyright © 2020 Nadaf, Imam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MatchesManagerTests/MatchesManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  MatchesManagerTests.swift
+//  MatchesManagerTests
+//
+//  Created by Nadaf, Imam on 09/05/20.
+//  Copyright © 2020 Nadaf, Imam. All rights reserved.
+//
+
+import XCTest
+@testable import MatchesManager
+
+class MatchesManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
